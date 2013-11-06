e5bd628ea747bb0ee3e4aab20f5e255d17abfdc5
SupahNickie <nicholascase@live.com>
2013-11-06 15:22:48 -0800

    Finished Chapter 5 plus chapter 4 exercises

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,29 @@
 class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pewps",
+      "attribution" => "You"
+    }
+    m = FileModel.create attrs
+    puts m.inspect
+    render :quote, :obj => m
+  end
+
   def a_quote
+    @verb = "go to church"
     render :a_quote, :noun => :winking
-
-    # ENV variable rendered below
-    # "\n<pre>\n#{env}\n</pre>"
   end
 
-  def exception
-    raise "It's a bad one!"
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
   end
+
 end
